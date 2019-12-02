1f01501e3770b9b1b61fd510421e97cef3ee6fd6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-12-02 00:11:55 +0000

    Replace deprated method

--- components/scope/navigation.php
+++ components/scope/navigation.php
@@ -1,5 +1,5 @@
 <nav class="s-navigation" role="navigation">
-<?php foreach ($items->visible() as $item): ?>
+<?php foreach ($items->listed() as $item): ?>
     <?php $title = ($item->title_short()->isNotEmpty()) ? $item->title_short() : $item->title(); ?>
     <a href="<?= $item->url() ?>"<?php e($item->isActive(), ' aria-current="page"') ?>><?= smartypants($title) ?></a>
 <?php endforeach ?>
